95b5fd67496a89c670a41b1adc3a0fcf000444f7
Cat Tran <trandinhcat@gmail.com>
2016-02-06 11:32:26 -0600

    readPMU6050

--- selfBallace/MPU6050.h
+++ selfBallace/MPU6050.h
@@ -24,6 +24,7 @@ class MPU6050{
 		//CONSTRUCTOR
 		MPU6050()
 		{
+			//Waking up MPU6050
 			Wire.begin();
   			Wire.beginTransmission(MPU_addr);
 			Wire.write(0x6B);  // PWR_MGMT_1 register
@@ -49,4 +50,22 @@ class MPU6050{
 			  gyroscopeLastValue = gyroscopeCurrentValue;
 		}
 
+		//Fetch data
+		void readMPU6050()
+		{
+			Wire.begin();
+			Wire.beginTransmission(MPU);
+			Wire.write(0x6B);  // PWR_MGMT_1 register
+			Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+			Wire.endTransmission(false);
+			Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+			//Read data from MPU6050
+			AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+			AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+			AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+			Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+			GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+			GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+			GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+		}
 };
