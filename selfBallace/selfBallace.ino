33d1fa50db2ce32eab4221be99b85a21f1172001
Cat Tran <trandinhcat@gmail.com>
2016-02-13 15:17:03 -0600

    Cleaned up the code a little bit...

--- selfBallace/selfBallace.ino
+++ selfBallace/selfBallace.ino
@@ -11,7 +11,7 @@
 #define kD  0//derivatives gain (calibratable)
 #define dt  .01//the time step over which integration is taken(in seccond)
 #define setAngle  90//value to approach
-#define SAMPLE_TIME 100
+#define SAMPLE_TIME 200
 //==================DECLARE SOME GLOBAL VARIABLES===================//
 int PWM;//pulse width modulation that will be send to controll motors' speed
 float static gyroscopeLastValue = 0;
@@ -20,36 +20,18 @@ float static gyroscopeFilteredAngle = 0;
 float static complementaryAngle;//the current measured angle
 float static previousAngle;//store value read from previous loop
 MPU6050 mpu6050Sensor;
+//PID controllerPID(mpu6050Sensor.getAccelerationY(), );
 //===================SETUP=========================================//
 void setup()
 {
-  mpu6050Sensor = MPU6050();
   mpu6050Sensor.initializeMPU6050();//waking up mpu6050
-  mpu6050Sensor.initializeAngleX();
-  mpu6050Sensor.initializeAngleY();
-  mpu6050Sensor.initializeAngleZ();
   mpu6050Sensor.setSampleTime(SAMPLE_TIME);
   Serial.begin(9600);
 }
 //=======================LOOP=======================================//
 void loop()
-{  
-  float x = mpu6050Sensor.getAccelerationX();
-  float y = mpu6050Sensor.getAccelerationY();
-  float z = mpu6050Sensor.getAccelerationZ();
-  float angleX = mpu6050Sensor.getAngleX();
+{ 
   float angleY = mpu6050Sensor.getAngleY();
-  float angleZ = mpu6050Sensor.getAngleZ();
-  // Serial.print(x);
-  // Serial.print(" ");
-  // Serial.print(y);
-  // Serial.print(" ");
-  // Serial.print(z);
-  // Serial.print("  ||  ");
-  Serial.print(angleX);
-  Serial.print(" ");
-  Serial.print(angleY);
-  Serial.print(" ");
-  Serial.println(angleZ);
+  Serial.println(angleY);
   delay(SAMPLE_TIME);
 }
